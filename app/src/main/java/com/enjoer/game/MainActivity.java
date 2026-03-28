21fa46bdc1d8103832d93db02a77f67cd533a049
andreibioazum-cell <andreibioazum@gmail.com>
2026-03-28 21:37:06 +0400

    Delete app directory

--- app/src/main/java/com/enjoer/game/MainActivity.java
+++
@@ -1,7 +0,0 @@
-package com.enjoer.game;
-// ... (все твои импорты из 6.1)
-
-public class MainActivity extends AppCompatActivity {
-    // Весь твой код управления, физики и JSON-экспорта
-    // Теперь он лежит в чистом .java файле
-}
